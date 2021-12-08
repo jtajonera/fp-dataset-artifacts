10e554d0b114e974c16154815ba29a0708ff2259
jtajonera1 <jtajonera@utexas.edu>
2021-12-07 22:13:21 -0600

    Adds augmented data and adversarial analysis script

--- run-ad-aug.py
+++ run-ad-aug.py
@@ -7,34 +7,51 @@ import os
 import json
 
 import textattack
-from textattack.augmentation import EmbeddingAugmenter
+from textattack.augmentation import EmbeddingAugmenter, CharSwapAugmenter, WordNetAugmenter
 
 NUM_PREPROCESSING_WORKERS = 2
 
 # To use augmented data run the following command: dataset = datasets.Dataset.from_csv("aug_data.csv").remove_columns('Unnamed: 0')
 
-def main():
-    # Dataset selection
+# To see effect of adversarial training run: textattack attack --recipe textfooler --model ./trained_model/ --num-examples 100 --dataset-from-huggingface snli
 
+# Dataset Size (multiple of 3)
+DATASET_SIZE = 3
+
+# Function to augment data
+def augmentData(augmenter, train_aug, start, end):
+    train_aug['premise'][start:end] = [augmenter.augment(train_aug['premise'][x])[0] for x in range(start, end)]
+    train_aug['hypothesis'][start:end] = [augmenter.augment(train_aug['hypothesis'][x])[0] for x in range(start, end)]  
+
+def main():
+
+    # Dataset selection
     dataset_id = 'snli'
     eval_split = 'validation'
     dataset = datasets.load_dataset("snli")
-
-    train_aug = dataset['train']
-
-    # Augments the first 20,000 cases in train
-    train_aug = train_aug[0:20000]
     
-    augmenter = EmbeddingAugmenter()
+    # Grab amount of data we would like to train
+    train_aug = dataset['train']
+    train_aug = train_aug[0:DATASET_SIZE]
 
-    train_aug['premise'] = [augmenter.augment(x)[0] for x in train_aug['premise']]
-    train_aug['hypothesis'] = [augmenter.augment(x)[0] for x in train_aug['hypothesis']]
+    # Initialize Augmenters
+    eAugmenter = EmbeddingAugmenter()
+    cAugmenter = CharSwapAugmenter()
+    wAugmenter = WordNetAugmenter()
 
-    print(type(train_aug))
+    step = int(DATASET_SIZE / 3)
 
+    # Augment data
+    augmentData(eAugmenter, train_aug, 0, step)
+    print("Completed 1/3")
+    augmentData(cAugmenter, train_aug, step, step * 2)
+    print("Completed 2/3")
+    augmentData(wAugmenter, train_aug, step * 2, step * 3)
+    print("Completed 3/3")
+
+    # Save data to CSV
     test = datasets.Dataset.from_dict(train_aug) 
-
-    test.to_csv("aug_data.csv")
+    test.to_csv("aug_data2.csv")
 
 if __name__ == "__main__":
     main()
--- text-attack.py
+++ text-attack.py
@@ -30,30 +30,3 @@ attack_args = textattack.AttackArgs(num_examples=20, log_to_csv="log.csv", check
 attacker = textattack.Attacker(attack, dataset, attack_args)
 attacker.attack_dataset()
 
-train_dataset = textattack.datasets.HuggingFaceDataset("snli", split="train")
-eval_dataset = textattack.datasets.HuggingFaceDataset("snli", split="test")
-
-# Code Inspired by: https://textattack.readthedocs.io/en/latest/api/trainer.html#trainer
-
-# Train for 3 epochs with 1 initial clean epochs, 1000 adversarial examples per epoch, learning rate of 5e-5, and effective batch size of 32 (8x4).
-training_args = textattack.TrainingArgs(
-    num_epochs=3,
-    num_clean_epochs=1,
-    num_train_adv_examples=1000,
-    learning_rate=5e-5,
-    per_device_train_batch_size=8,
-    gradient_accumulation_steps=4,
-    log_to_tb=True,
-)
-
-trainer = textattack.Trainer(
-    model_wrapper,
-    "classification",
-    attack,
-    train_dataset,
-    eval_dataset,
-    training_args
-)
-
-trainer.train()
-
