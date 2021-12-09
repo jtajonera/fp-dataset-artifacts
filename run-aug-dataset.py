817591ebc007aaae876849767580813fa3869904
jtajonera1 <jtajonera@utexas.edu>
2021-12-08 21:29:52 -0600

    Adds training script

--- run-aug-dataset.py
+++ run-aug-dataset.py
@@ -9,19 +9,22 @@ import json
 import textattack
 from textattack.augmentation import EmbeddingAugmenter, CharSwapAugmenter, WordNetAugmenter
 
-NUM_PREPROCESSING_WORKERS = 2
-
 # To use augmented data run the following command: dataset = datasets.Dataset.from_csv("aug_data.csv").remove_columns('Unnamed: 0')
 
 # To see effect of adversarial training run: textattack attack --recipe textfooler --model ./trained_model/ --num-examples 100 --dataset-from-huggingface snli
 
 # Dataset Size (multiple of 3)
-DATASET_SIZE = 3
+DATASET_SIZE = 500000
 
 # Function to augment data
 def augmentData(augmenter, train_aug, start, end):
-    train_aug['premise'][start:end] = [augmenter.augment(train_aug['premise'][x])[0] for x in range(start, end)]
-    train_aug['hypothesis'][start:end] = [augmenter.augment(train_aug['hypothesis'][x])[0] for x in range(start, end)]  
+    # train_aug['premise'][start:end] = [augmenter.augment(train_aug['premise'][x])[0] for x in range(start, end)]
+    # train_aug['hypothesis'][start:end] = [augmenter.augment(train_aug['hypothesis'][x])[0] for x in range(start, end)]  
+
+    for x in range(start, end):
+        train_aug['premise'][x] = augmenter.augment(train_aug['premise'][x])[0]
+        train_aug['hypothesis'][x] = augmenter.augment(train_aug['hypothesis'][x])[0]
+
 
 def main():
 
@@ -32,26 +35,28 @@ def main():
     
     # Grab amount of data we would like to train
     train_aug = dataset['train']
+
+    print(train_aug)
     train_aug = train_aug[0:DATASET_SIZE]
 
     # Initialize Augmenters
     eAugmenter = EmbeddingAugmenter()
-    cAugmenter = CharSwapAugmenter()
+    # cAugmenter = CharSwapAugmenter()
     wAugmenter = WordNetAugmenter()
 
-    step = int(DATASET_SIZE / 3)
+    step = int(DATASET_SIZE / 2)
 
     # Augment data
     augmentData(eAugmenter, train_aug, 0, step)
-    print("Completed 1/3")
-    augmentData(cAugmenter, train_aug, step, step * 2)
-    print("Completed 2/3")
-    augmentData(wAugmenter, train_aug, step * 2, step * 3)
-    print("Completed 3/3")
+    print("Completed 1/2")
+    # augmentData(cAugmenter, train_aug, step, step * 2)
+    # print("Completed 2/2")
+    augmentData(wAugmenter, train_aug, step, step * 2)
+    print("Completed 2/2")
 
     # Save data to CSV
     test = datasets.Dataset.from_dict(train_aug) 
-    test.to_csv("aug_data2.csv")
+    test.to_csv("aug_data.csv")
 
 if __name__ == "__main__":
     main()
---
+++ run-aug-train.py
@@ -0,0 +1,96 @@
+import datasets
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, \
+    AutoModelForQuestionAnswering, Trainer, TrainingArguments, HfArgumentParser
+from helpers import prepare_dataset_nli, prepare_train_dataset_qa, \
+    prepare_validation_dataset_qa, QuestionAnsweringTrainer, compute_accuracy
+import os
+import json
+
+import textattack
+
+# Run the following command: python3 run-aug-train.py --model ./trained_model/ --output_dir ./outputs/ --dataset snli
+
+def main():
+    argp = HfArgumentParser(TrainingArguments)
+
+    argp.add_argument('--model', type=str,
+                      default='google/electra-small-discriminator',
+                      help="""This argument specifies the base model to fine-tune.
+        This should either be a HuggingFace model ID (see https://huggingface.co/models)
+        or a path to a saved model checkpoint (a folder containing config.json and pytorch_model.bin).""")
+    argp.add_argument('--dataset', type=str, default=None,
+                      help="""This argument overrides the default dataset used for the specified task.""")
+                      
+    training_args, args = argp.parse_args_into_dataclasses()
+    # Dataset selection
+    if args.dataset.endswith('.json') or args.dataset.endswith('.jsonl'):
+        dataset_id = None
+        # Load from local json/jsonl file
+        dataset = datasets.load_dataset('json', data_files=args.dataset)
+        # By default, the "json" dataset loader places all examples in the train split,
+        # so if we want to use a jsonl file for evaluation we need to get the "train" split
+        # from the loaded dataset
+        eval_split = 'train'
+    if args.dataset.endswith('.csv'):
+        dataset_id = None
+        print(dataset)
+        dataset = datasets.Dataset.from_csv(args.dataset).remove_columns('Unnamed: 0') 
+        # raise Exception()
+        eval_split = 'train'
+    else:
+        default_datasets = {'qa': ('squad',), 'nli': ('snli',)}
+        dataset_id = tuple(args.dataset.split(':')) if args.dataset is not None else \
+            default_datasets[args.task]
+        # MNLI has two validation splits (one with matched domains and one with mismatched domains). Most datasets just have one "validation" split
+        eval_split = 'validation_matched' if dataset_id == ('glue', 'mnli') else 'validation'
+        # Load the raw data
+        dataset = datasets.load_dataset(*dataset_id)
+    
+    # NLI models need to have the output label count specified (label 0 is "entailed", 1 is "neutral", and 2 is "contradiction")
+    task_kwargs = {'num_labels': 3} 
+
+    # Initialize the model and tokenizer from the specified pretrained model/checkpoint
+    model = AutoModelForSequenceClassification.from_pretrained(args.model, **task_kwargs)
+    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=True)
+
+    print("Preprocessing data... (this takes a little bit, should only happen once per dataset)")
+    if dataset_id == ('snli',):
+        # remove SNLI examples with no label
+        dataset = dataset.filter(lambda ex: ex['label'] != -1)
+    
+    train_dataset = dataset['train']
+    eval_dataset = dataset[eval_split]
+
+    training_args = textattack.TrainingArgs(
+        num_epochs=3,
+        num_clean_epochs=0,
+        num_train_adv_examples=1000,
+        learning_rate=0.00001,
+        per_device_train_batch_size=8,
+        gradient_accumulation_steps=4,
+        log_to_tb=True,
+    )   
+
+    model_wrapper = textattack.models.wrappers.HuggingFaceModelWrapper(model, tokenizer)
+    
+    format_data_train = [((train_dataset[x]['premise'], train_dataset[x]['hypothesis']), train_dataset[x]['label']) for x in range(len(train_dataset))]#len(train_dataset))
+    format_data_eval = [((eval_dataset[x]['premise'], eval_dataset[x]['hypothesis']), eval_dataset[x]['label']) for x in range(len(eval_dataset))]#len(train_dataset))
+
+    # print(format_data_train)
+    tData = textattack.datasets.Dataset(format_data_train, input_columns=("premise", "hypothesis"))
+    eData = textattack.datasets.Dataset(format_data_eval, input_columns=("premise", "hypothesis"))
+    attack = textattack.attack_recipes.DeepWordBugGao2018.build(model_wrapper)
+
+    trainer = textattack.Trainer(
+        model_wrapper,
+        "classification",
+        attack,
+        tData,
+        eData,
+        training_args
+    )
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
